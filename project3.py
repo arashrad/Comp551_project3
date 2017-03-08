78e391292519ab538e7fcd660e136dbce93b012c
gregorylaredo <gregory.laredo@mail.mcgill.ca>
2017-03-08 16:54:12 -0500

    latest version (March 8)

--- project3.py
+++ project3.py
@@ -17,9 +17,8 @@ import skimage.color
 #import theano
 import keras
 from keras.models import Sequential
-from keras.layers import Activation
-from keras.layers import Dense
-from keras.optimizers import SGD
+from keras.layers import Activation, Dense, Conv2D, pooling, Reshape, Flatten, Dropout, BatchNormalization
+from keras.optimizers import SGD, Adam
 from keras.utils import np_utils
 import imutils
 from imutils import paths
@@ -32,13 +31,18 @@ trainY = np.load("/Users/gregorylaredo/Desktop/tinyY.npy")
 testX = np.load("/Users/gregorylaredo/Desktop/tinyX_test.npy") # (6600, 3, 64, 64)
 
 # to visualize only
-plt.imshow(testX[0].transpose(2,1,0)) # put RGB channels last
+plt.imshow(testX[10].transpose(2,1,0)) # put RGB channels last
 plt.show()
 #x = trainX[1]
 #len(x)
 #
 #x_reshape = np.reshape(x,[64,64,3])
 trainX_reshape = np.reshape(trainX,[len(trainX),64,64,3])
+
+
+# Probability of each Class
+cat,count = np.unique(trainY,return_counts=True)
+prob = count/len(trainY)
 
 
 
@@ -53,42 +57,148 @@ def image_to_feature_vector(image,size=(64,64)):
 #plt.imshow(SIFT_image)
 #plt.show()
 
+
+
+
 # ========== Randomly shuffling the data and splitting into training and validation sets
 X_train, X_val, y_train, y_val = train_test_split(trainX_reshape, trainY, test_size=0.2, random_state=42)
 
-# first decided to test implementation on Sample Set of around 1% of images
-X_train_sample_set = X_train[0:2600][:][:][:]
-y_train_sample_set = y_train[0:2600]
-# ^ Note that I checked to seethat all 40 classes are present in sample set
+## first decided to test implementation on Sample Set of around 1% of images
+#X_train_sample_set = X_train[0:10000][:][:][:]
+#y_train_sample_set = y_train[0:10000]
+## ^ Note that I checked to seethat all 40 classes are present in sample set
 
-data = []
-labels = y_train_sample_set
+data_train = []
+#labels = y_train
 
-for (i,X_train_sample_set) in enumerate(X_train_sample_set):
-    features = image_to_feature_vector(X_train_sample_set)
-    data.append(features)
-    
-data = np.array(data) / 255.0
-labels = np_utils.to_categorical(labels)
+for (i,X_train) in enumerate(X_train):
+    features = image_to_feature_vector(X_train)
+    data_train.append(features)
+data_train = np.array(data_train) / 255.0
+#labels = np_utils.to_categorical(labels)
 
-# going to separate a training and validation set from the sample set (which happened to be part of the training set already separated above)
-(trainData, testData, trainLabels, testLabels) = train_test_split(data, labels, test_size=0.2, random_state=42)
+data_val = []
+for (i,X_val) in enumerate(X_val):
+    features = image_to_feature_vector(X_val)
+    data_val.append(features)
+data_val = np.array(data_val) / 255.0
+
+## going to separate a training and validation set from the sample set (which happened to be part of the training set already separated above)
+#(trainData, testData, trainLabels, testLabels) = train_test_split(data, labels, test_size=0.2, random_state=42)
+
+trainX_reshape_random, x, trainY_random, y = train_test_split(trainX_reshape, trainY, test_size=0.0, random_state=42)
+data_ALL_train = []
+for (i,trainX_reshape_random) in enumerate(trainX_reshape_random):
+    features = image_to_feature_vector(trainX_reshape_random)
+    data_ALL_train.append(features)
+data_ALL_train = np.array(data_ALL_train) / 255.0
+y_ALL_train = trainY_random
 
 # ======== Keras Package for simple NN
 model = Sequential()
-model.add(Dense(1536,input_dim=12288,init="uniform",activation="relu"))
-model.add(Dense(768,init="uniform",activation="relu"))
-model.add(Dense(40))
+model.add(Dense(output_dim=512,input_dim=12288,activation="relu")) # Hidden Layer 1
+model.add(Dense(output_dim=512,activation="relu")) # HL 2
+model.add(Dense(output_dim=512,activation="relu")) # HL 3
+model.add(Dense(output_dim=512,activation="relu")) # HL 4
+model.add(Dense(output_dim=256,activation="relu")) # HL 5
+model.add(Dense(output_dim=40))
 model.add(Activation("softmax"))
 
 # Training the model via Stochastic Mini-Batch Gradient Descent (SGD)
 print("[INFO] compiling model...")
-sgd = SGD(lr=0.01)
-model.compile(loss="binary_crossentropy",optimizer=sgd,metrics=["accuracy"])
-model.fit(trainData, trainLabels, nb_epoch=50, batch_size=128)
+#sgd = SGD(lr=0.01)
+BS = 256 # batch size
+model.compile(loss="sparse_categorical_crossentropy",
+              optimizer=Adam(lr=0.001),
+              metrics=["accuracy"])
+history = model.fit(data_ALL_train, y_ALL_train, 
+                    validation_split=0.20, 
+                    nb_epoch=20, batch_size=BS)
+# summarize history for accuracy
+plt.plot(history.history['acc'])
+plt.plot(history.history['val_acc'])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
+## summarize history for loss
+#plt.plot(history.history['loss'])
+#plt.plot(history.history['val_loss'])
+#plt.title('model loss')
+#plt.ylabel('loss')
+#plt.xlabel('epoch')
+#plt.legend(['train', 'test'], loc='upper left')
+#plt.show()
+
+# Can't do below when fitting to all data and already testing on a validation set in the .fit code
 print("[INFO] evaluating on test set...")
-(loss,accuracy) = model.evaluate(testData, testLabels, batch_size=128, verbose=1)
+(loss,accuracy) = model.evaluate(data_val, y_val, batch_size=BS, verbose=1)
 print("[INFO] loss={:.4f}, accuracy: {:.4f}%".format(loss,accuracy*100))
+
+
+# ======== Keras Package for Convolutional NN
+model_CNN = Sequential()
+model_CNN.add(Reshape((3,64,64), input_shape=(12288,))) # HL 1
+model_CNN.add(Conv2D(64,3,3, border_mode='same', subsample=(4,4), activation='relu')) # HL 2
+model_CNN.add(pooling.AveragePooling2D(border_mode='same')) # HL 3
+model_CNN.add(Conv2D(32,3,3, border_mode='same', activation='relu'))
+#model_CNN.add(BatchNormalization())
+#model_CNN.add(Dropout(0.5))
+model_CNN.add(Flatten())
+model_CNN.add(Dense(output_dim=256, activation='relu')) # HL 4
+#model_CNN.add(BatchNormalization())
+model_CNN.add(Dropout(0.5))
+model_CNN.add(Dense(output_dim=40, activation='softmax'))
+print("[INFO] compiling model...")
+BS = 256
+model_CNN.compile(loss="sparse_categorical_crossentropy",
+              optimizer=Adam(),
+              metrics=["accuracy"])
+history = model_CNN.fit(data_ALL_train, y_ALL_train, 
+              validation_split=0.20, 
+              nb_epoch=50, batch_size=BS)
+# summarize history for accuracy
+plt.plot(history.history['acc'])
+plt.plot(history.history['val_acc'])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
+## summarize history for loss
+#plt.plot(history.history['loss'])
+#plt.plot(history.history['val_loss'])
+#plt.title('model loss')
+#plt.ylabel('loss')
+#plt.xlabel('epoch')
+#plt.legend(['train', 'test'], loc='upper left')
+#plt.show()
+
+
+# ======= Predict on left-over of original training set based on above Model
+X_to_predict = X_train[15001:21075][:][:][:]
+y_to_predict = y_train[15001:21075]
+data_to_predict = []
+for (i,X_to_predict) in enumerate(X_to_predict):
+    features = image_to_feature_vector(X_to_predict)
+    data_to_predict.append(features)
+data_to_predict = np.array(data_to_predict) / 255.0
+
+pred = model_CNN.predict(data_to_predict, batch_size=BS, verbose=1)
+pred_cat = np.zeros((len(pred),1))
+for i in range(len(pred)):
+    temp = pred[i,:]
+    pred_cat[i] = np.where(temp == temp.max())
+
+y_pred = pred_cat    
+y_true = y_to_predict 
+from sklearn.metrics import metrics
+print (metrics.classification_report(y_true, y_pred))
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_true, y_pred)
+plt.matshow(cm)
+
 
 # ======= Predict on Test Set based on above Model
 testX_reshape = np.reshape(testX,[len(testX),64,64,3])
@@ -98,7 +208,7 @@ for (i,testX_reshape) in enumerate(testX_reshape):
     testX_data.append(features)
 testX_data = np.array(testX_data) / 255.0
 
-pred = model.predict(testX_data, batch_size=128, verbose=1)
+pred = model.predict(testX_data, batch_size=BS, verbose=1)
 
 pred_cat = np.zeros((len(pred),1))
 for i in range(len(pred)):
@@ -106,7 +216,7 @@ for i in range(len(pred)):
     pred_cat[i] = np.where(temp == temp.max())
     
 # Export to CSV file
-np.savetxt("prediction_1%trainingset_kerasNN_2Hlayers.csv",pred_cat)
+np.savetxt("prediction_15000trainingset_kerasCNN_3Hlayers_f1score.csv",pred_cat)
 
 
 
